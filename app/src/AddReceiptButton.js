cb1b86fc81973e891fefeae953b075681b7cd533
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-04-20 19:27:54 -0400

    NavBar dependencies

--- app/src/AddReceiptButton.js
+++ app/src/AddReceiptButton.js
@@ -4,10 +4,29 @@ import AddIcon from '@material-ui/icons/Add';
 
 class AddReceiptButton extends Component {
 
+  state = {
+    selector: null,
+    file: null
+  }
+
+  componentDidMount = () => {
+    this.setState({selector: document.createElement('input')});
+  }
+
+  fileSelect = () => {
+    this.state.selector.setAttribute('type', 'file');
+    this.state.selector.click();
+  }
+
+  fileUploaded(){
+    console.log('file')
+  }
+
   render(){
+
     return (
       <div>
-        <Fab color="primary" aria-label="Add" style={styles.button}>
+        <Fab color="primary" aria-label="Add" style={styles.button} onClick={() => {this.fileSelect()}}>
           <AddIcon />
         </Fab>
       </div>
