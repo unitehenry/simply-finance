3cf4ab11f462030fab8ea73d8f71a9274cde547c
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-04-20 17:39:55 -0400

    new menu

--- app/src/NavBar.js
+++ app/src/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 
@@ -8,17 +8,12 @@ function NavBar(props){
     <AppBar position="static">
       <Toolbar>
         {
-          props.auth ?
-          (
-            null
-          ) :
-          <IconButton color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
+          ['transactions', 'budget','about', 'sign out'].map((item) => {
+            return (
+              <Button style={styles.button}>{item}</Button>
+            )
+          })
         }
-        <Typography variant="h6" color="inherit" style={styles.header}>
-          SimpliFi
-        </Typography>
       </Toolbar>
     </AppBar>
   )
@@ -27,6 +22,9 @@ function NavBar(props){
 const styles = {
   header: {
     margin: '1em'
+  },
+  button: {
+    color: 'inherit'
   }
 }
 
