9522a6ae2250b3ab69a12f7d6db80e4622fa292a
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-04-20 16:50:04 -0400

    Transactions

--- app/src/Transactions.js
+++ app/src/Transactions.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
+import { Typography, Card, CardContent, CardActions, Button } from '@material-ui/core';
 
 class Transations extends Component {
 
   render(){
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div style={{padding: '1.5em'}}>
+
+        <div>
+          <Typography variant="h4" color="inherit" style={styles.header}>
+              Total Spent
+          </Typography>
+          <Typography variant="h3" color="inherit" style={styles.header}>
+              { "$35.26" }
+          </Typography>
+        </div>
+
+        {Transaction()}
+        {Transaction()}
+        {Transaction()}
+        {Transaction()}
+        {Transaction()}
+        {Transaction()}
+        {Transaction()}
+
       </div>
     )
   }
 
 }
 
-const styles = {
+function Transaction(){
+  return(
+    <Button style={{width: '100%'}}>
+      <Card style={{width: '100%'}}>
+        <CardContent>
+          <Typography variant="h6" color="inherit" style={styles.transaction.header}>
+              { "Vendor Name - $35.26" }
+          </Typography>
+        </CardContent>
+      </Card>
+    </Button>
+  )
+}
 
+const styles = {
+  header: {
+    textAlign: 'center',
+    margin: '.3em'
+  },
+  transaction: {
+    header: {
+
+    }
+  }
 }
 
 export default Transations;
