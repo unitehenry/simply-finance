1c33f22e6b8443810630a8883defea780a730c9d
Henry Unite <henryunite@Henrys-MacBook-Pro.local>
2019-04-20 16:36:00 -0400

    icons and navbar

---
+++ app/src/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+function NavBar(){
+  return(
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" style={styles.header}>
+          SimpliFi
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const styles = {
+  header: {
+    margin: '1em'
+  }
+}
+
+export default NavBar;
---
+++ app/src/Transactions.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Transations extends Component {
+
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+
+}
+
+const styles = {
+
+}
+
+export default Transations;
