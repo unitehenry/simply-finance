3ffab80cb7f2fa1bff943301d45bd4130580a15a
dsbuddy <des338@cs.drexel.edu>
2019-04-20 21:03:23 -0400

    transcribe done

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var fs = require('fs');
 var request = require('request');
 
 const tesseract = require('node-tesseract-ocr');
+var imgur = require('imgur');
 
-
-var TesseractModule = require('./server/tesseract.js');
 
 /* Set up server */
 app.use(express.static("."));
@@ -17,17 +17,47 @@ app.use(bodyParser.json());
 
 
 
-app.get('/transcribeReceipt', function(req, res) {
-	var query = req.query;
-	var path = query.path;
 
-	var text = TesseractModule.transcribe(path);
-	if (res == "") {
-		console.log("Unable to transcribe picture");
-	} else {
-		res.send(text);
+
+app.get('/transcribeReceipt', function(req, res) {
+	// Link : https://i.imgur.com/tElLPXP.png
+	var query = req.query;
+	// var path = query.path;
+	var url = query.image;
+	console.log(url);
+
+
+	var filename = './resources/imgs/pic.' + url.split('.').pop();
+
+	var writeFileStream = fs.createWriteStream(filename);request(url).pipe(writeFileStream).on('close', function() {
+	  console.log(url, 'saved to', filename);
+	})
+
+
+
+	const config = {
+		lang: 'eng',
+		oem: 0,
+		psm: 4
 	}
 
+	
+	var promise = tesseract.recognize(filename, config);
+
+	promise.then(function(value){
+		var jsonObj = [];
+		var lines = value.split("\n");
+		lines.forEach(function(line){
+			if (/[0-9]/.test(line.slice(-1))) {
+				var firstNum = line.match(/\d/);
+				var content = [line.substring(0,line.indexOf(firstNum)), line.substring(line.indexOf(firstNum))];
+				var temp = {"Name" : content[0], "Price" : content[1]};
+				jsonObj.push(temp);
+			}
+		});
+		console.log(jsonObj);
+		res.send(jsonObj);
+	})
 });
 
 
