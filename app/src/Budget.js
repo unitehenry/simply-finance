ddf475afc43e023577f5b74820fc95eb672a156f
Henry Unite <henryunite@n3-94-120.dhcp.drexel.edu>
2019-04-21 14:27:39 -0400

    categories

--- app/src/Budget.js
+++ app/src/Budget.js
@@ -27,121 +27,47 @@ class Budget extends Component {
     db.collection("users").doc(this.props.uid)
       .get().then(function(doc) {
         if (doc.exists) {
-            const data = doc.data();
-            const resultPercent = calcPercent(JSON.stringify(doc.data().transactions));
-            component.setState({transactions: data.transactions, percent: resultPercent})
+            component.setState({transactions: doc.data().transactions})
+            console.log(component.state.categories);
+            component.getCategories();
         } else {
             console.log("No such document!");
         }
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
+  }
 
-    // console.log(this.state.transactions)
-    //console.log(calcPercent(JSON.stringify(this.state.transactions[1].items)))
+  getCategories = () => {
+    let categories = [];
+
+    this.state.transactions.forEach((transaction) => {
+      transaction.items.forEach((item) => {
+        if(categories.includes(item.Category)){
+          return;
+        } else{
+          categories.push(item.Category);
+
+        }
+      })
+    })
+
+    this.setState({categories: categories})
   }
 
   render() {
     return (
-      <div>
-
-        {!this.state.questions ?
-            (
-
-            <div style = {styles.header}>
-              <Progress percent={this.state.percent} />
-              <Button variant="contained" style={styles.button2} onClick={()=> {this.setState({graphs: true,questions: true,nameTextField: false,header: false})}} style={styles.button}>
-                Create!
-              </Button>
-              <Button variant="contained" id = 'Edit'  style={styles.button}>
-                Edit
-              </Button>
-            </div>
-          )
-            :
-            (
-            <div>
-              <Questions updateItem={(item) => this.select(item)}/>
-            </div>
-            )
-          }
-        </div>
-    );
+      <div style={styles.content}>
+        <Progress categories={this.state.categories}/>
+      </div>
+    )
   }
 }
 
 const styles = {
-  header: {
-    textAlign: 'center',
-    margin: '1em',
-    fontSize: 30,
-  },
-  info: {
-    textAlign: 'center'
-  },
-  inBut:{
-      visibility: 'false'
-  },
-  center:{
-    textAlign: 'center',
-    fontSize: 50
-  },
-  button2:{
-      margin: 'lem'
-  },
-  button: {
-    margin: '1em'
+  content: {
+    padding: '1em'
   }
 }
-
-function calcPercent(oldJSON){
-      var dict = {}
-      var someObj = [];
-      var js = JSON.parse(oldJSON);
-      for (var item in js) {
-        for (var elm in js[item].items) {
-          // console.log(js[item].items[elm]);
-          someObj.push(js[item].items[elm]);
-        }
-        // console.log(item);
-        // console.log(js[item]);
-      }
-
-
-      for (var i = 0; i < someObj.length;i++){
-        if (someObj[i].Category != "") {
-          if (someObj[i].Category in dict ){
-              if (isNaN(someObj[i].Price)) {
-                dict[someObj[i].Category] += 0;
-              } else {
-                dict[someObj[i].Category] += parseInt(someObj[i].Price, 10);
-              }
-          }else {
-              if (isNaN(someObj[i].Price)) {
-                dict[someObj[i].Category] = 0;
-              } else {
-                dict[someObj[i].Category] = parseInt(someObj[i].Price, 10);
-              }
-          }
-        }
-      }
-      var count = 0
-      var percent = {}
-      for (var key in dict){
-        if (key != "") {
-          if (isNaN(dict[key])) {
-                  count += 0
-          } else {
-              count += parseInt(dict[key], 10);
-          }
-        }
-      }
-      for (var key in dict){
-          percent[key] = parseInt(dict[key]/count*100);
-      }
-
-      return percent;
-
-  }
 
 export default Budget;
--- app/src/Progress.js
+++ app/src/Progress.js
@@ -6,19 +6,29 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 class Progress extends React.Component {
-
   render() {
-    console.log(this.props.percent);
+    return (
+      <div style={styles.roots}>
+        <Paper style={styles.paper} elevation={5}>
 
-    return(
-      <React.Fragment>
-        {
-          Object.keys(this.props.percent).forEach(function(key){
-            return(<h1>key</h1>)
-          })
-        }
-      </React.Fragment>
-    )
+          {
+
+            this.props.categories.map((category) => {
+              return (
+                <React.Fragment key={category}>
+                  <Typography variant="h5" component="h3" style={{padding: '1em'}}>
+                    <label>{ category === '' ? 'Other' : category }</label>
+                    <LinearProgress variant="determinate" value={98.99}/>
+                  </Typography>
+                  <br />
+                </React.Fragment>
+              )
+            })
+
+          }
+        </Paper>
+      </div>
+    );
   }
 }
 
@@ -36,14 +46,11 @@ const styles = theme => ({
   },
   paper: {
     flexGrow: 1,
-    padding: '1.5em',
+    padding: '3em 1.5em 1.5em 1.5em',
     textAlign: 'left'
   },
   progressBar: {
     margin: '1em'
-  },
-  labelTag: {
-    padding: '1em'
   }
 });
 
